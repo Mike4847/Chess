50349f07dde7a2f9afbb2174b9e9c28a957795ce
Mike4847 <michaelelemanl19@students.tukenya.ac.ke>
2025-08-29 09:41:57 +0300

    code refactoring

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # CHESS GAME 
+![Under Construction](https://img.shields.io/badge/Status-Under%20Construction-orange?style=for-the-badge&logo=construction)
+
+Using the above project as a practise project so anybosy can clone the project and well does anything to it.
+
+
+The ideas to be implemented are as follows.(not a complete list)
+- [X] Drawing the table i.e chess table and bliting the chess pieces. 
+- [X] Implementing movements in relation specific piece type.
+- [ ] 2 people playable game _use of sockets_.
+- [ ] "___maybe implement the application` as a web game___"
--- chess/ChessEngine.py
+++ chess/ChessEngine.py
@@ -13,13 +13,14 @@ class GameState():
             ["0", "0", "0", "0", "0", "0", "0", "0"],
             ["0", "0", "0", "0", "0", "0", "0", "0"],
             ["0", "0", "0", "0", "0", "0", "0", "0"],
-            ["0", "0", "0", "0", "0", "0", "0", "0"],
+            ["0", "0", "0", "bp", "0", "0", "0", "0"],
             ["wp","wp","wp","wp","wp","wp","wp","wp"],
             ["wR","wN","wB","wQ", "wK","wB","wN","wR"]
         ]
         self.WhiteToMove = True
         self.moveLog = []
-        self.MapPieceToMove = {"B": self.BishopMove, "K": self.kingMove, "Q":self.QueenMove, "p": self.PawnMove, "N": self.KNightMove, "R":self.RookMove}
+        self.pieceFunction = {"B": self.BishopMove, "K": self.kingMove, "Q":self.QueenMove, "p": self.PawnMove, "N": self.KNightMove, "R":self.RookMove}
+        
 
     def makeMove(self,move):
         self.board[move.startRow][move.startCol] = "0"
@@ -30,7 +31,7 @@ class GameState():
 
     def undoMove(self):
         """
-        !!(NOTE) to undo a move is basically returning the state of the board from where initialy was.
+        !!(NOTE) to undo a move is basically returning the state of the board from what initialy was.
 
         """
         if(len(self.moveLog) > 0):
@@ -43,17 +44,29 @@ class GameState():
     def PawnMove(self,row, col, move):
         if (self.board[row][col][0] == "w"): #white attack moves
             #check if the 
-            if((row - 1) >= 0):
-                if(self.board[row-1][col] == "0"):
-                    move.append(Move((row, col), (row - 1, col), self.board))
-                    if (row== 6 and self.board[row - 2][col] == "0"):
-                        move.append(Move((row, col), (row - 2, col), self.board))
-            if ((col + 1) <= 7):
-                if(self.board[row][col + 1] != "0"):
+            if (self.board[row-1][col] == "0"):
+                move.append(Move((row, col), (row - 1, col), self.board))
+                if (row== 6 and self.board[row - 2][col] == "0"):
+                    move.append(Move((row, col), (row - 2, col), self.board))
+            if ((col + 1) <= len(self.board)): # check for right captures 
+                if(self.board[row - 1][col + 1][0] == "b"):
                     move.append(Move((row, col), (row-1, col + 1), self.board))
-            if (col - 1>= 0):
-                if(self.board[row][col - 1] != "0"):
+            if (col - 1>= 0):# check for left captures
+                if(self.board[row][col - 1][0] == "b"):
                     move.append(Move((row, col), (row-1, col - 1), self.board))
+        else: #implementing the black moves
+            if (row + 1) > len(self.board): # move one step forward
+                move.append((row, col), (row + 1, col))
+                if row == 1 and self.board[row+2][col] == "0": # but whatif its a pawns first move and the path is clear ... move twice!!
+                    move.append((row, col),(row + 2, col))
+            # NOTE(CAPTURES)
+            if (col + 1) >=0 and self.board[row + 1][col + 1][0] =="w":     # right captures
+                move.append(Move((row , col), (row+1,col+1), self.board))
+            if ()
+
+
+
+            
 
                   
 
@@ -69,12 +82,15 @@ class GameState():
         for row in range(len(self.board)):
             for col in range(len(self.board[0])):
                 if((self.board[row][col][0] == "w" and self.WhiteToMove ) or (self.board[row][col][0] == "b" and not self.WhiteToMove ) ):
-                    self.MapPieceToMove[self.board[row][col][1]](row, col, moves)
+                    #piece = self.board[r][c]
+                    self.pieceFunction[self.board[row][col][1]](row, col, moves)
                 
         return moves
     
     def BishopMove(self,row , col, move):
-        pass
+        if self.board[row][col][0] == "w": #white move
+            
+    
     
 
     def KNightMove(self,row , col, move):
@@ -106,9 +122,9 @@ class Move():
         self.movedPiece = board[self.startRow][self.startCol]
         self.CapturedPiece = board[self.endRow][self.endCol]
         self.PieceNotation = ""
-
-        if (len(self.movedPiece) == 2 and self.movedPiece[1] != "p"):
-            self.PieceNotation = self.movedPiece[1]
+        #attach a unique identifier
+        self.MoveId = self.startCol *10000 + self.endCol * 1000 + self.endRow*100
+        self.PieceNotation = self.movedPiece[1]
         
 
 
@@ -131,7 +147,7 @@ class Move():
         i am gonna try as much as possible to keep the notation as the : ALGEBRA NOTATION
         """
 
-        return print(f"From square:--> {self.getFilRank(self.startRow, self.startCol)}\t.To square :-->{self.getFilRank(self.endRow, self.endCol)}")
+        return print(f"From square:--> {self.getFilRank(self.startRow, self.startCol)} {self.PieceNotation}\t.To square :-->{self.getFilRank(self.endRow, self.endCol)}{self.PieceNotation}")
         
 
 
